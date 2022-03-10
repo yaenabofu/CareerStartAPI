cef7f17f412920c852c6e856d80fbe770b98e576
AntonBazhutin <nezzterlivejuke@gmail.com>
2022-03-10 11:58:10 +0500

    y

--- WebApi/Models/Login.cs
+++ WebApi/Models/Login.cs
@@ -6,6 +6,7 @@ namespace WebApi.Models
     {
         public string Email { get; set; }
         public string Password { get; set; }
+
         public string RefreshToken { get; set; }
         public DateTime RefreshTokenExpiryTime { get; set; }
     }
